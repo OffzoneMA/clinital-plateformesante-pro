93a8d20047782ff7550ffc5e02a7ef4c05b91d39
zakariacheknane <cheknanezakaria@gmail.com>
2024-06-01 00:05:43 +0100

    update style

--- src/components/cabinet/PublishProfil2.jsx
+++ src/components/cabinet/PublishProfil2.jsx
@@ -4,36 +4,36 @@ import MenuCabinet from "../menuCabinet/MenuCabinet";
 
 function PublishProfil2() {
   return (
-    <div className="publishProfil">
+    <div className="publishProfilP">
       <div className="result-container">
         <MenuCabinet state="4" />
       </div>
-      <div className="container">
-      <div className="div">
-        <div className="div-2">
-          <div className="titre">Publier mon profil</div>
-          <p className="text-wrapper">2 - Publier mon Profil</p>
+      <div className="containerP">
+      <div className="divP">
+        <div className="div-2P">
+          <div className="titreP">Publier mon profil</div>
+          <p className="text-wrapperP">2 - Publier mon Profil</p>
         </div>
-        <div className="div-3">
-          <img className="ellipse" alt="Ellipse" src="../../icons/doctor.svg" />
-          <div className="div-4">
-            <div className="text-wrapper-2">Dr Démos CLINITAL</div>
-            <div className="text-wrapper-3">Médecin généraliste</div>
+        <div className="div-3P">
+          <img className="ellipseP" alt="Ellipse" src="../../icons/doctor.svg" />
+          <div className="div-4P">
+            <div className="text-wrapper-2P">Dr Démos CLINITAL</div>
+            <div className="text-wrapper-3P">Médecin généraliste</div>
           </div>
         </div>
       </div>   
-      <hr className="line" alt="Line"  />
-      <div className="div-5">
-        <img className="img" alt="Frame" src="../../icons/Frame.svg" />
-        <div className="div-6">
-          <div className="flexcontainer">
-          <p className="text">
-            <span className="span">
+      <hr className="lineP" alt="Line"  />
+      <div className="div-5P">
+        <img className="imgP" alt="Frame" src="../../icons/Frame.svg" />
+        <div className="div-6P">
+          <div className="flexcontainerP">
+          <p className="textP">
+            <span className="spanP">
             Bienvenue sur Clinital ! <br/>
             </span>
           </p>
-          <p className="text">
-          <span className="text-wrapper-4">
+          <p className="textP">
+          <span className="text-wrapper-4P">
           En cliquant sur le bouton ci-dessous, vous rendrez votre profil visible
             <br />
            à tous les utilisateurs. 
@@ -42,10 +42,10 @@ function PublishProfil2() {
             </span>
           </p>
           </div>
-          <div className="button">
-            <div className="button-2">
-              <div className="titre-2">Publier</div>
-              <img className="plus-circle" alt="plus-circle" src="../../icons/flech-white.svg" />
+          <div className="buttonP">
+            <div className="button-2P">
+              <div className="titre-2P">Publier</div>
+              <img className="plus-circleP" alt="plus-circle" src="../../icons/flech-white.svg" />
             </div>
           </div>
         </div>
--- src/components/cabinet/PublishProfilSucces.jsx
+++ src/components/cabinet/PublishProfilSucces.jsx
@@ -4,24 +4,24 @@ import MenuCabinet from "../menuCabinet/MenuCabinet";
 
 function PublishProfilSucces() {
   return (
-    <div className="publishProfil">
+    <div className="publishProfilS">
       <div className="result-container">
         <MenuCabinet state="4" />
       </div>
-      <div className="container">
-        <p className="text-wrapper">
+      <div className="containerS">
+        <p className="text-wrapperS">
           Félicitations ! <br/>
           Votre profil a été publié avec succès
         </p>
-        <img className="check" alt="Check" src="../icons/check.svg"/>
-        <div className="group">
-          <div className="overlap-group">
-          <div className="button">
-            <div className="button-2">
-              <div className="titre-2">Voir mon profil</div>
+        <img className="checkS" alt="Check" src="../icons/check.svg"/>
+        <div className="groupS">
+          <div className="overlap-groupS">
+          <div className="buttonS">
+            <div className="button-2S">
+              <div className="titre-2S">Voir mon profil</div>
             </div>
           </div>
-          <img className="funny" alt="Funny" src="../images/suc_mdp-img.png"/>
+          <img className="funnyS" alt="Funny" src="../images/suc_mdp-img.png"/>
           </div>
         </div>
       </div>
